d6557f826bd50a167846685e4ed3e71421947026
sugino <git@sugi.cc>
2021-04-06 12:58:52 +0900

    add UIs

---
+++ Assets/ArtNetController/Scripts/UI/EditableDropdownField.cs
@@ -0,0 +1,31 @@
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class EditableDropdownField : VisualElement
+{
+    public new class UxmlFactory : UxmlFactory<EditableDropdownField, UxmlTraits> { }
+    public new class UxmlTraits : VisualElement.UxmlTraits
+    {
+        UxmlStringAttributeDescription m_Choices = new UxmlStringAttributeDescription() { name = "choices", defaultValue = "element1,element2,element3" };
+        public override void Init(VisualElement ve, IUxmlAttributes bag, CreationContext cc)
+        {
+            base.Init(ve, bag, cc);
+            var field = (EditableDropdownField)ve;
+            field.AddChoices(m_Choices.GetValueFromBag(bag, cc).Split(',').Select(e => e.Trim()));
+        }
+    }
+    List<string> m_choices;
+    public void ClearChoices() => m_choices.Clear();
+    public void AddChoices(IEnumerable<string> choices) => m_choices.AddRange(choices);
+    DropdownField m_dropdownField;
+
+    public EditableDropdownField()
+    {
+        m_choices = new List<string> { "first",};
+        m_dropdownField = new DropdownField(m_choices, 0);
+        Add(m_dropdownField);
+    }
+}
--- Assets/ArtNetController/Scripts/UI/XYPad.cs
+++ Assets/ArtNetController/Scripts/UI/XYPad.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -24,52 +22,6 @@ public class XYPad : VisualElement
         }
     }
 
-    public XYPad() : base()
-    {
-        var upSide = new VisualElement();
-        var bottomSide = new VisualElement();
-        var xParam0 = new VisualElement();
-        var xParam1 = new VisualElement();
-
-        Add(upSide);
-        Add(bottomSide);
-        upSide.Add(xParam0);
-        bottomSide.Add(xParam1);
-
-        style.backgroundColor = Color.white;
-
-        upSide.style.flexDirection = bottomSide.style.flexDirection = FlexDirection.Row;
-        upSide.style.flexGrow = upSide.style.flexShrink = 1;
-
-        bottomSide.AddToClassList("paramY");
-        bottomSide.style.flexGrow = bottomSide.style.flexShrink = 0;
-        bottomSide.style.borderTopColor = Color.black;
-        bottomSide.style.borderTopWidth = 1;
-        bottomSide.style.height = Length.Percent(50);
-
-        xParam0.AddToClassList("paramX");
-        xParam1.AddToClassList("paramX");
-        this.Query(null, "paramX").ForEach(e =>
-        {
-            e.style.flexGrow = e.style.flexShrink = 0;
-            e.style.width = Length.Percent(50);
-            e.style.borderRightColor = Color.black;
-            e.style.borderRightWidth = 1;
-        });
-
-        RegisterCallback<PointerDownEvent>(SetPointerValue);
-        RegisterCallback<PointerMoveEvent>(SetPointerValue);
-    }
-    void SetPointerValue(IPointerEvent p)
-    {
-        if ((p.pressedButtons & 1) == 0)
-            return;
-        var pos = p.localPosition;
-        var (w, h) = (localBound.width, localBound.height);
-        var v2 = Value;
-        (v2.x, v2.y) = (pos.x / w, 1f - pos.y / h);
-        Value = v2;
-    }
     public Vector2 Value
     {
         get => m_value;
@@ -77,21 +29,52 @@ public class XYPad : VisualElement
         {
             value.x = Mathf.Clamp01(value.x);
             value.y = Mathf.Clamp01(value.y);
+            lineX.style.left = (localBound.width - 1) * value.x;
+            lineY.style.bottom = (localBound.height - 1) * value.y;
             m_value = value;
-            this.Query(null, "paramX").ForEach(e => e.style.width = Length.Percent(value.x * 100));
-            this.Q(null, "paramY").style.height = Length.Percent(value.y * 100);
-            if (onChange != null)
-                onChange.Invoke(m_value);
+            onChange?.Invoke(m_value);
         }
     }
+    public void SetValueWithoutNotify(Vector2 v2) => m_value = v2;
     Vector2 m_value;
-    public System.Action<Vector2> onChange;
+    public event System.Action<Vector2> onChange;
     public Color LineColor
     {
-        set
+        set => lineX.style.backgroundColor = lineY.style.backgroundColor = value;
+    }
+
+    VisualElement lineX;
+    VisualElement lineY;
+
+    public XYPad() : base()
+    {
+        lineX = new VisualElement();
+        lineY = new VisualElement();
+        lineX.style.width = lineY.style.height = 1;
+        lineX.style.height = lineY.style.width = Length.Percent(100f);
+        lineX.style.position = lineY.style.position = Position.Absolute;
+        lineX.style.backgroundColor = lineY.style.backgroundColor = Color.black;
+        Add(lineX);
+        Add(lineY);
+
+        RegisterCallback<PointerDownEvent>(evt =>
         {
-            this.Q(null, "paramY").style.borderTopColor = value;
-            this.Query(null, "paramX").ForEach(e => e.style.borderRightColor = value);
-        }
+            this.CapturePointer(evt.pointerId);
+            SetPointerValue(evt);
+        });
+        RegisterCallback<PointerMoveEvent>(evt =>
+        {
+            if (this.HasPointerCapture(evt.pointerId))
+                SetPointerValue(evt);
+        });
+        RegisterCallback<PointerUpEvent>(evt => this.ReleasePointer(evt.pointerId));
+    }
+    void SetPointerValue(IPointerEvent evt)
+    {
+        var pos = evt.localPosition;
+        var (w, h) = (localBound.width, localBound.height);
+        var v2 = Value;
+        (v2.x, v2.y) = (pos.x / w, 1f - pos.y / h);
+        Value = v2;
     }
 }
